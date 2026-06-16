7f4f74e764eaf4af839e76c89c32a029449d60a9
Mark Kinyanjui <mckinyanjui3@gmail.com>
2026-06-16 15:49:19 +0300

    fixed the root error

--- generate_log.py
+++ generate_log.py
@@ -1,26 +1,4 @@
-from datetime import datetime
-import requests
-
-
-def generate_log(data):
-
-    if not isinstance(data, list):
-        raise ValueError("Input must be a list")
-
-    filename = f"log_{datetime.now().strftime('%Y%m%d')}.txt"
-
-    with open(filename, "w") as file:
-        for entry in data:
-            file.write(f"{entry}\n")
-
-    return filename
-
-
-def fetch_data():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
-    if response.status_code == 200:
-        return response.json()
-    return {}
+from lib.generate_log import fetch_data, generate_log
 
 
 if __name__ == "__main__":
